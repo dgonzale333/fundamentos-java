27b95a73750b777d7eaf61a54c39fa0d49e7efda
David <davidgonzalezdiaz2305@gmail.com>
2024-07-25 11:26:06 -0500

    Avance hasta el ejercicio 6

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static int ejercicio1() {
+        int x = (int) 5.89 + (int) 3.45;
+        return x;
+    }
+
+    public static  int ejercicio2() {
+        char c = 'A';
+        int entero = (int) c;
+        return entero;
+    }
+
+    public static int ejercicio3 (){
+        double d = 100.04;
+        long l = (long) d;
+        int i = (int) l;
+        return i;
+    }
+
+    public static int ejercicio4() {
+        String text = "40";
+        int s = Integer.parseInt(text);
+        return s;
+    }
+
+    public static int ejercicio5() {
+        byte =-1;
+        String s = String(20);
+        int num1 = 20;
+    }
+
+    public static int ejercicio6(){
+        String text = String(65);
+        int num = 65;
+    }
+
+
+
     }
 }
---
+++ src/solve/Solve.java
@@ -0,0 +1,8 @@
+
+public class Solve {
+    public static void main(String[] args) {
+
+
+
+    }
+}
